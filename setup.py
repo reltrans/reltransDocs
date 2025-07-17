b88bcf6d417f272068a395266112e60e145e299d
Matteo Lucchini <m.lucchini@uva.nl>
2025-07-17 12:12:28 +0200

    The commits will continue until builds improve

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['numpy']
+
+test_requirements = [ ]
+
+setup(
+    author="Matteo Lucchini",
+    author_email='m.lucchini@uva.nl',
+    python_requires='>=3.8',
+    classifiers=[
+        'Intended Audience :: End Users',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.10',
+    ],
+    description="The reltrans model documentation.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='reltrans',
+    name='reltrans',
+    #packages=find_packages(include=['ndspec']),
+    #test_suite='tests',
+    #tests_require=test_requirements,
+    url='git@github.com:reltrans/reltransDocs.git',
+    version='2.3.0',
+    zip_safe=False,
+)
